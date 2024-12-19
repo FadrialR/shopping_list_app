286f35b6f37bfc8d5b3024504b974ac42f8a94e8
Hikmat Fadrial Ramadhan <hikmatfadrial2000@gmail.com>
2024-12-19 13:57:36 +0700

    Adding a 'New Item' screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shopping_list_app/widgets/grocery_list.dart';
 
-final theme = ThemeData(
-  useMaterial3: true,
-  colorScheme: ColorScheme.fromSeed(
-    brightness: Brightness.dark,
-    seedColor: const Color.fromARGB(255, 131, 57, 0),
-  ),
-  textTheme: GoogleFonts.latoTextTheme(),
-);
-
 void main() {
   runApp(const MyApp());
 }
@@ -24,7 +15,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Groceries',
-      theme: theme,
+      theme: ThemeData.dark().copyWith(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 147, 229, 250),
+          brightness: Brightness.dark,
+          surface: const Color.fromARGB(255, 42, 51, 59),
+        ),
+        scaffoldBackgroundColor: const Color.fromARGB(255, 50, 58, 60),
+        textTheme: GoogleFonts.poppinsTextTheme(),
+      ),
       home: const GroceryList(),
     );
   }
