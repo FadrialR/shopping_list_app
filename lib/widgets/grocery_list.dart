4c5c00839664943c18818816cf05bbf0fea6c2a1
Hikmat Fadrial Ramadhan <hikmatfadrial2000@gmail.com>
2024-12-21 19:15:33 +0700

    Fix the screen when there are no items

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -28,8 +28,47 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
+  // void _removeItem(GroceryItem item) {
+  //   setState(() {
+  //     _groceryItems.remove(item);
+  //   });
+  // }
+
   @override
   Widget build(BuildContext context) {
+    Widget content = const Center(
+      child: Text('No items added yet.'),
+    );
+
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (ctx, index) => ListTile(
+            title: Text(_groceryItems[index].name),
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: _groceryItems[index].category.color,
+            ),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  _groceryItems[index].quantity.toString(),
+                ),
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _groceryItems.removeAt(index);
+                    });
+                  },
+                  icon: const Icon(Icons.delete),
+                )
+              ],
+            )),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
@@ -40,50 +79,7 @@ class _GroceryListState extends State<GroceryList> {
           )
         ],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          if (_groceryItems.isEmpty)
-            const Center(
-              child: Text(
-                'There\'s no item here...',
-                style: TextStyle(
-                  fontSize: 18,
-                ),
-                textAlign: TextAlign.left,
-              ),
-            )
-          else
-            Expanded(
-              child: ListView.builder(
-                itemCount: _groceryItems.length,
-                itemBuilder: (ctx, index) => ListTile(
-                    title: Text(_groceryItems[index].name),
-                    leading: Container(
-                      width: 24,
-                      height: 24,
-                      color: _groceryItems[index].category.color,
-                    ),
-                    trailing: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text(
-                          _groceryItems[index].quantity.toString(),
-                        ),
-                        IconButton(
-                          onPressed: () {
-                            setState(() {
-                              _groceryItems.removeAt(index);
-                            });
-                          },
-                          icon: const Icon(Icons.delete),
-                        )
-                      ],
-                    )),
-              ),
-            ),
-        ],
-      ),
+      body: content,
     );
   }
 }
