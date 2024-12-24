9f7f21b7c2d031a8a118e2f056485e1f0f9b7a0d
Hikmat Fadrial Ramadhan <hikmatfadrial2000@gmail.com>
2024-12-24 16:05:31 +0700

    Error message when deleting an item

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -73,14 +73,22 @@ class _GroceryListState extends State<GroceryList> {
   }
 
   void _removeItem(GroceryItem item) async {
-    final url = Uri.https('flutter-prep-79247-default-rtdb.firebaseio.com',
-        'shopping-list/${item.id}.json');
-
-    http.delete(url);
+    final index = _groceryItems.indexOf(item);
 
     setState(() {
       _groceryItems.remove(item);
     });
+
+    final url = Uri.https('flutter-prep-79247-default-rtdb.firebaseio.com',
+        'shopping-list/${item.id}.json');
+
+    final response = await http.delete(url);
+
+    if (response.statusCode >= 400) {
+      setState(() {
+        _groceryItems.insert(index, item);
+      });
+    }
   }
 
   @override
